f3deed11921f4808b333c421d8d6dff3a06a40a7
KuroKugo <KuroKugo@users.noreply.github.com>
2017-03-08 13:41:26 -0500

    Updated Parser to Create a Tree
    Parser now creates trees and outputs them to the console.
    Lexer and index.html are uploaded to most recent files.

--- Parser.js
+++ Parser.js
@@ -18,12 +18,15 @@ function parse(testchar) {
         //console.log(e.message);
     }
     $('#result').append(PrintResult());
-    
+    var tree = csTree.toString();
+    console.log(tree);
+   // $('#result').append(tree);
 }
 
 //parse functions
 function parseProgram () {
-    
+    //create program node
+    csTree.addNode("Program", "branch");
     
     parseBlock();
     //match $
@@ -32,6 +35,8 @@ function parseProgram () {
 }
 
 function parseBlock () {
+    csTree.addNode("Block", "branch");
+    
     //match {
     match("LBRACE");
     
@@ -42,9 +47,12 @@ function parseBlock () {
     //match }
     match("RBRACE");
     
+    csTree.endChildren();
 }
 
 function parseStatementList () {
+    csTree.addNode("StatmentList", "branch");
+    
     if(tokenList[currentIndex].kind != "RBRACE") {
             parseStatement();
     
@@ -54,9 +62,12 @@ function parseStatementList () {
             // do nothing for epsilon
         }
     
+    csTree.endChildren();
 }
 
 function parseStatement () {
+    csTree.addNode("Statement", "branch");
+    
     if(tokenList[currentIndex].kind == "PRINT") {
         parsePrintStatement();
     } else if(tokenList[currentIndex].kind == "ID") {
@@ -74,9 +85,13 @@ function parseStatement () {
         //console.log("Error : expexted \" print, id, a datatype, while, if, or {, recived \" " + tokenList[currentIndex].charValue + " at line " + tokenList[currentIndex].lineNum + "\n");
         throw "Error : expexted \" print, id, a datatype, while, if, or {\" recived  " + tokenList[currentIndex].charValue + " at line " + tokenList[currentIndex].lineNum + "\n";
     }
+    
+    csTree.endChildren();
 }
 
 function parsePrintStatement() {
+    csTree.addNode("PrintStatement", "branch");
+    
     //match print
     match("PRINT");
     
@@ -88,9 +103,12 @@ function parsePrintStatement() {
     //match )
     match("RPAREN");
     
+    csTree.endChildren();
 }
 
 function parseAssignmentStatement () {
+    csTree.addNode("AssignmentStatement", "branch");
+    
     parseId();
     
     //match =
@@ -98,26 +116,36 @@ function parseAssignmentStatement () {
     
     parseExpr();
     
+    csTree.endChildren();
 }
 
 function parseVarDecl () {
+    csTree.addNode("VarDecl", "branch");
+    
     //match type
     match("DATATYPE");
     
     parseId();
     
+    csTree.endChildren();
 }
 
 function parseWhileStatement () {
+    csTree.addNode("WhileStatement", "branch");
+    
     //match while
     match("WHILE");
     
     parseBooleanExpr();
     
     parseBlock();
+    
+    csTree.endChildren();
 }
 
 function parseIfStatement () {
+    csTree.addNode("IfStatement", "branch");
+    
     //match if
     match("IF");
     
@@ -125,9 +153,12 @@ function parseIfStatement () {
     
     parseBlock();
     
+    csTree.endChildren();
 }
 
 function parseExpr () {
+    csTree.addNode("Expr", "branch");
+    
     if(tokenList[currentIndex].kind == "DIGIT") {
         parseIntExpr();
     } else if(tokenList[currentIndex].kind == "QUOTE") {
@@ -139,9 +170,13 @@ function parseExpr () {
     } else {
         throw "Error : expexted \" a digit, \", (, a boolean value, or an id\" recived  " + tokenList[currentIndex].charValue + " at line " + tokenList[currentIndex].lineNum + "\n";;
     }
+    
+    csTree.endChildren();
 }
 
 function parseIntExpr () {
+    csTree.addNode("IntExpr", "branch");
+    
     //match digit
     match("DIGIT");
     
@@ -151,9 +186,13 @@ function parseIntExpr () {
     
         parseExpr();   
     }  
+    
+    csTree.endChildren();
 }
 
 function parseStringExpr () {
+    csTree.addNode("StringExpr", "branch");
+    
     //match quote
     match("QUOTE");
     
@@ -162,9 +201,12 @@ function parseStringExpr () {
     //match quote
     match("QUOTE");
     
+    csTree.endChildren();
 }
 
 function parseBooleanExpr () {
+    csTree.addNode("BooleanExpr", "branch");
+    
     if(tokenList[currentIndex].kind == "LPAREN") {
     //match (
         match("LPAREN");
@@ -184,14 +226,22 @@ function parseBooleanExpr () {
     } else {
         throw "Error : expexted \" ( or a boolean value\" recived  " + tokenList[currentIndex].charValue + " at line " + tokenList[currentIndex].lineNum + "\n";;
     }
+    
+    csTree.endChildren();
 }
 
 function parseId () {
+    csTree.addNode("Id", "branch");
+    
     //match Id
     match("ID");
+    
+    csTree.endChildren();
 }
 
 function parseCharList () {
+    csTree.addNode("CharList", "branch");
+    
     if (tokenList[currentIndex].kind == "CHAR") { 
     //match char
         match("CHAR");
@@ -200,14 +250,123 @@ function parseCharList () {
     } else {
         //do nothing for epsilon   
     }
+    
+    csTree.endChildren();
 }
 
 function match (expectedToken) {
     if(expectedToken == tokenList[currentIndex].kind) {
-        result = result.concat("Good  on "+ expectedToken +"\n");
+        //result = result.concat("Good  on "+ expectedToken +"\n");
+        csTree.addNode(tokenList[currentIndex].charValue, "leaf");
         currentIndex++;
     } else {        
         //result = result.concat("Error : Expected "+ expectedToken + " recieved " + tokenList[currentIndex].kind + "at line " + tokenList[currentIndex].lineNum + "\n");
         throw "Error : Expected "+ expectedToken + " recieved " + tokenList[currentIndex].kind + " at line " + tokenList[currentIndex].lineNum + "\n";
     }
+}
+
+//-----------------------------------------
+// treeDemo.js
+//
+// By Alan G. Labouseur, based on the 2009
+// work by Michael Ardizzone and Tim Smith.
+//-----------------------------------------
+
+function Tree() {
+    // ----------
+    // Attributes
+    // ----------
+    
+    this.root = null;  // Note the NULL root node of this tree.
+    this.cur = {};     // Note the EMPTY current node of the tree we're building.
+
+
+    // -- ------- --
+    // -- Methods --
+    // -- ------- --
+
+    // Add a node: kind in {branch, leaf}.
+    this.addNode = function(name, kind) {
+        // Construct the node object.
+        var node = { name: name,
+                     children: [],
+                     parent: {}
+                   };
+
+        // Check to see if it needs to be the root node.
+        if ( (this.root == null) || (!this.root) )
+        {
+            // We are the root node.
+            this.root = node;
+        }
+        else
+        {
+            // We are the children.
+            // Make our parent the CURrent node...
+            node.parent = this.cur;
+            // ... and add ourselves (via the unfrotunately-named
+            // "push" function) to the children array of the current node.
+            this.cur.children.push(node);
+        }
+        // If we are an interior/branch node, then...
+        if (kind == "branch")
+        {
+            // ... update the CURrent node pointer to ourselves.
+            this.cur = node;
+        }
+    };
+
+    // Note that we're done with this branch of the tree...
+    this.endChildren = function() {
+        // ... by moving "up" to our parent node (if possible).
+        if ((this.cur.parent !== null) && (this.cur.parent.name !== undefined))
+        {
+            this.cur = this.cur.parent;
+        }
+        else
+        {
+            console.log("Why am i getting here");
+            // TODO: Some sort of error logging.
+            // This really should not happen, but it will, of course.
+        }
+    };
+
+    // Return a string representation of the tree.
+    this.toString = function() {
+        // Initialize the result string.
+        var traversalResult = "";
+
+        // Recursive function to handle the expansion of the nodes.
+        function expand(node, depth)
+        {
+            // Space out based on the current depth so
+            // this looks at least a little tree-like.
+            for (var i = 0; i < depth; i++)
+            {
+                traversalResult += "-";
+            }
+
+            // If there are no children (i.e., leaf nodes)...
+            if (!node.children || node.children.length === 0)
+            {
+                // ... note the leaf node.
+                traversalResult += "[" + node.name + "]";
+                traversalResult += "\n";
+            }
+            else
+            {
+                // There are children, so note these interior/branch nodes and ...
+                traversalResult += "<" + node.name + "> \n";
+                // .. recursively expand them.
+                for (var i = 0; i < node.children.length; i++)
+                {
+                    expand(node.children[i], depth + 1);
+                }
+            }
+        }
+        // Make the initial call to expand from the root.
+        expand(this.root, 0);
+        // Return the result.
+        return traversalResult;
+    };
 }
